7b5198a6b79aab625512d88c6258e70f35802fc8
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-03-27 22:23:52 -0400

    Form Validation, Refetch Tweets on Submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,41 +6,40 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1679687034042
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1679773434042
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1679687034042
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1679773434042
+  //   }
+  // ]
 
   const daysAgo = function(date) {
     const now = Math.floor(new Date().getTime())
-    console.log(now);
     return Math.round((now - date) / (24 * 3600));
   };
 
-
   const renderTweets = function(tweets) {
     // loops through tweets
-    for (let tweet of data) {
+    $('.tweet-container').empty();
+    for (let tweet of tweets.reverse()) {
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
       $('.tweet-container').append(createTweetElement(tweet));
@@ -48,9 +47,8 @@ $(document).ready(function() {
   };
 
   const createTweetElement = function(tweetObject) {
-    console.log("createdAT", tweetObject.created_at);
     const daysPosted = daysAgo(tweetObject.created_at);
-    const $tweet = $(`<article>
+    const $tweet = $(`<article class="tweet-article">
     <header class="tweet-header">
     <div>
     <p><img class="image" src="${tweetObject.user.avatars}"/> ${tweetObject.user.name}</p>
@@ -71,11 +69,47 @@ $(document).ready(function() {
     </div>
     </footer>
     </article>
+    <br>
     `);
     return $tweet
   };
   
-renderTweets(data);
+  const loadTweet = () => {
+    $.ajax({
+      url: "/tweets",
+      action: "GET"
+    })
+    .then((res) => {
+      
+
+      renderTweets(res);
+    })
+  }
+  $('form').on("submit", (event) => {
+    event.preventDefault();
+    const data = $("form").serialize()
+    const $text = $("#tweet-text").val();
+    if ($text.length > 140) {
+      alert("Please, keep short, there is a limit of 140 characters")
+    }else if (!$text){
+      alert("If you want to Tweet, you need to write something!")
+    } else {
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data
+      })
+     .then(() => {
+      loadTweet()
+     })
+  
+      $("#form-id").trigger("reset");
+    }
+  })
+  loadTweet();
 
 });
+
+
+
 
